16b82316add52b7ec0a179b62c064ed486972f91
Andrew Kirby <andrewkirby22@engs-29609.eng.ox.ac.uk>
2023-11-06 14:57:59 +0000

    Calculate thrust coefficient

---
+++ calculate_ct_star_single_turbine.py
@@ -0,0 +1,72 @@
+"""Calculate `internal' turbine thrust
+coefficient for large-eddy simulations
+of a single turbine
+"""
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+case_id = 'H1000-C5-G4_st'
+
+#vertical grid - use cell centered points  
+with open('/mnt/d/LES_data/zmesh','r') as file:       
+    Nz_full     = int(float(file.readline()))   
+    N_line = Nz_full+1
+    line = N_line*[0]
+    cnt = 0
+    for lines in file:
+        line[cnt]=lines.split()
+        cnt+=1       
+    z = np.array([float(line[k][0]) for k in range(int(Nz_full))])[1::2]
+
+#calculate the average turbine orientation
+aux = h5py.File(f'/mnt/d/LES_data/{case_id}/aux_files.h5', 'r')
+yaw = aux['yaw']
+time = aux['time']
+#mean yaw angle in degrees
+yaw_max = np.mean(yaw[time[:]>75600],axis=0)
+yaw_max = np.max(yaw_max)
+print("Maximum yaw angle is ", yaw_max, "degrees")
+yaw_mean = np.mean(yaw[time[:]>75600])
+
+#check farm area is correct
+f = h5py.File(f'/mnt/d/LES_data/{case_id}/stat_main_first_order.h5', 'r')
+u = f['u']
+v = f['v']
+plt.close()
+plt.pcolormesh(u[:240,:,24])
+plt.colorbar()
+plt.axvline(230, c='blue')
+plt.savefig('plots/farm_area.png')
+
+#calculate CV-averaged quantities
+u_ave = 59*np.mean(u[:240,:,:59]) + 0.5*np.mean(u[:240,:,59])
+u_ave = u_ave/59.5
+print(u_ave)
+v_ave = 59*np.mean(v[:240,:,:59]) + 0.5*np.mean(v[:240,:,59])
+v_ave = v_ave/59.5
+print(v_ave)
+
+plt.close()
+u_profile = np.mean(u[:240,:,:60], axis=(0,1))
+plt.plot(u_profile, 1000*z[:60])
+plt.axvline(u_ave)
+plt.axhline(119)
+plt.savefig('plots/u_profile.png')
+
+#calculate U_F
+u_f = u_ave*np.cos(np.pi*yaw_mean/180) + v_ave*np.sin(np.pi*yaw_mean/180)
+print(u_f)
+
+#calculate turbine force in direction of average turbine orientation
+yaw_diff = yaw - yaw_mean
+force = aux['force']
+force_component = force*np.cos(np.pi*yaw_diff/180)
+force_ave = np.mean(force_component[time[:]>75600,:])
+print(force_ave)
+
+#calculate internal turbine thrust coefficient
+turbine_area = (np.pi*198**2)/4
+ct_star = force_ave / (0.5*turbine_area*u_f**2)
+print(ct_star)
--- read_LES_data.py
+++ read_LES_data.py
@@ -2,14 +2,10 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-f = h5py.File('/mnt/c/Users/trin3517/Documents/PhD/Year 4/Research plots and presentations/LES_data/H500-C5-G4/stat_main_first_order.h5', 'r')
+f = h5py.File('/mnt/d/LES_data/H1000-C5-G4/aux_files.h5', 'r')
 
-cmap = plt.get_cmap('seismic')
-p = f['p']
-p = np.mean(p[:,460:920,:300], axis=1) - np.mean(p[0,460:920,:300], axis=0)
+force = f['force']
+time = f['time']
 
-x = np.arange(0,50000,31.25)
-z = np.arange(2.5,1500,5)
-plt.figure(figsize=[8,1])
-plt.pcolormesh(x, z, p.T, cmap=cmap, shading='nearest', vmin=-18, vmax=18)
-plt.savefig('p_slice_H500-C5-G4.png')
+plt.plot(time[:], force[:,100])
+plt.savefig('plots/turbine_force.png')
